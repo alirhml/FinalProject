b34481c479a97b9c28ce1c879fc33856369e68c1
alirhml <arehimli50@gmail.com>
2024-04-02 20:39:20 +0400

    init commit

---
+++ src/components/home/Footer/FooterBottom.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { AiOutlineCopyright } from "react-icons/ai";
+
+const FooterBottom = () => {
+  return (
+    <div className="w-full bg-[#FF0000] group">
+     
+    </div>
+  );
+};
+
+export default FooterBottom;
